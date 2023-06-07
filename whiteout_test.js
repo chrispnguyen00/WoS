0f0b8dd21d168c6ed75e5da14d5a695f781233ab
Chris Nguyen <39972578+chrispnguyen00@users.noreply.github.com>
2023-06-06 21:52:09 -0700

    Add files via upload

--- whiteout_test.js
+++ whiteout_test.js
@@ -1,3 +1,6 @@
+//Global Variables needed
+var appPackage = "com.gof.global"
+
 if(Android.connected()) {
     Helper.log("Found Device, trying to start bot...");
     main();
@@ -5,3 +8,10 @@ if(Android.connected()) {
     Helper.log("No device connected!");
 }
 
+function main() {
+    Helper.log("Welcome to WoS Farm Bot v1");
+    Helper.log("Checking if the game is installed...");
+    Helper.log("Trying to start the game...");
+    Android.startApp(appPackage);
+    //gameLoop();
+}
